f6d84ed07c6c8c66e44b951493a4c6e4afd409f8
Colin Jamison <colin.jamison@liatrio.com>
2025-09-22 12:21:15 -0700

    chore: finish renames

--- internal/provider/client/engineers.go
+++ internal/provider/client/engineers.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-// GetCoffees - Returns list of coffees (no auth required)
+// GetEngineers - Returns list of engineers (no auth required)
 func (c *Client) GetEngineers() ([]Engineer, error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("%s/engineers", c.HostURL), nil)
 	if err != nil {
@@ -28,9 +28,9 @@ func (c *Client) GetEngineers() ([]Engineer, error) {
 	return engineers, nil
 }
 
-// GetCoffee - Returns specific coffee (no auth required)
-func (c *Client) GetEngineer(coffeeID string) ([]Engineer, error) {
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/engineer/%s", c.HostURL, coffeeID), nil)
+// GetEngineer - Returns specific engineer (no auth required)
+func (c *Client) GetEngineer(engineerID string) ([]Engineer, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/engineers/%s", c.HostURL, engineerID), nil)
 	if err != nil {
 		return nil, err
 	}
@@ -49,14 +49,14 @@ func (c *Client) GetEngineer(coffeeID string) ([]Engineer, error) {
 	return engineers, nil
 }
 
-// CreateCoffee - Create new coffee
-func (c *Client) CreateCoffee(engineer Engineer) (*Engineer, error) {
+// CreateEngineer - Create new Engineer
+func (c *Client) CreateEngineer(engineer Engineer) (*Engineer, error) {
 	rb, err := json.Marshal(engineer)
 	if err != nil {
 		return nil, err
 	}
 
-	req, err := http.NewRequest("POST", fmt.Sprintf("%s/coffees", c.HostURL), strings.NewReader(string(rb)))
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/engineers", c.HostURL), strings.NewReader(string(rb)))
 	if err != nil {
 		return nil, err
 	}
